afa284a1f598bc3a60769bf699adec6672c43887
YUHAN QIAN <yh984980263@gmail.com>
2021-05-29 01:49:42 +0800

    remove some useless line

--- src/main/java/exceptions/FormatException.java
+++ src/main/java/exceptions/FormatException.java
@@ -1,8 +1,5 @@
 package exceptions;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class FormatException extends RuntimeException {
-    private static final Logger logger = LogManager.getLogger(FormatException.class);
+
 }
--- src/main/java/exceptions/IncompatibleTypeException.java
+++ src/main/java/exceptions/IncompatibleTypeException.java
@@ -1,4 +1,4 @@
 package exceptions;
 
-public class IncompatibleTypeException extends NullPointerException{
+public class IncompatibleTypeException extends RuntimeException{
 }
